63143ec28282db5846e269e09785fd1cec1ec398
Daniel Hudson <35522603+DanielHudson2@users.noreply.github.com>
2022-01-16 17:20:24 +0000

    added extra parameters to movies

--- hooked/src/components/Movie.js
+++ hooked/src/components/Movie.js
@@ -3,10 +3,14 @@ import React from "react";
 const DEFAULT_PLACEHOLDER_IMAGE =
   "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
 
+const IMDB_URL_OPENER = "https://www.imdb.com/title/";
+
 
 const Movie = ({ movie }) => {
   const poster =
     movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
+
+  const imdbUrl = IMDB_URL_OPENER + movie.imdbID;
   return (
     <div className="movie">
       <h2>{movie.Title}</h2>
@@ -18,6 +22,7 @@ const Movie = ({ movie }) => {
         />
       </div>
       <p>({movie.Year})</p>
+      <p><a href={`${imdbUrl}`} target="_blank">View on IMDB</a></p>
     </div>
   );
 };
